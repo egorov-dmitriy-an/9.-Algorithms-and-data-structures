e4262f432cd23fe861f13b78725e6b99287cdfec
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2023-04-03 14:09:03 +0300

    Seminar4

---
+++ Seminar4/src/Tree.java
@@ -0,0 +1,108 @@
+public class Tree<V extends Comparable<V>> {
+    private Node root;
+
+    public boolean add(V value) {
+        if (root != null) {
+            boolean result = addNode(root, value);
+            root = balance(root);
+            root.color = Color.BLACK;
+            return result;
+        } else {
+            root = new Node();
+            root.color = Color.BLACK;
+            root.value = value;
+            return true;
+        }
+    }
+
+    public class Node {
+        private V value;
+        private Color color;
+        private Node leftChild;
+        private Node rightChild;
+    }
+
+    private boolean addNode(Node node, V value) {
+        if (node.value == value) {
+            return false;
+        } else {
+            if (node.value.compareTo(value) > 0) {
+                if (node.leftChild != null) {
+                    boolean result = addNode(node.leftChild, value);
+                    node.leftChild = balance(node.leftChild);
+                    return result;
+                } else {
+                    node.leftChild = new Node();
+                    node.leftChild.color = Color.RED;
+                    node.leftChild.value = value;
+                    return true;
+                }
+            } else {
+                if (node.rightChild != null) {
+                    boolean result = addNode(node.rightChild, value);
+                    node.rightChild = balance(node.rightChild);
+                    return result;
+                } else {
+                    node.rightChild = new Node();
+                    node.rightChild.color = Color.RED;
+                    node.rightChild.value = value;
+                    return true;
+                }
+            }
+        }
+    }
+
+    private Node balance(Node node) {
+        Node result = node;
+        boolean needRebalance;
+        do {
+            needRebalance = false;
+            if (result.rightChild != null && result.rightChild.color == Color.RED
+                    && (result.leftChild == null || result.leftChild.color == Color.BLACK)) {
+                needRebalance = true;
+                result = rightSwap(result);
+            }
+            if (result.leftChild != null && result.leftChild.color == Color.RED
+                    && result.leftChild.leftChild != null && result.leftChild.leftChild.color == Color.RED) {
+                needRebalance = true;
+                result = leftSwap(result);
+            }
+            if (result.leftChild != null && result.leftChild.color == Color.RED
+                    && result.rightChild != null && result.rightChild.color == Color.RED) {
+                needRebalance = true;
+                colorSwap(result);
+            }
+        } while (needRebalance);
+        return result;
+    }
+
+    private Node leftSwap(Node node) {
+        Node leftChild = node.leftChild;
+        Node betweenChild = leftChild.rightChild;
+        leftChild.rightChild = node;
+        node.leftChild = betweenChild;
+        leftChild.color = node.color;
+        node.color = Color.RED;
+        return leftChild;
+    }
+
+    private Node rightSwap(Node node) {
+        Node rightChild = node.rightChild;
+        Node betweenChild = rightChild.leftChild;
+        rightChild.leftChild = node;
+        node.rightChild = betweenChild;
+        rightChild.color = node.color;
+        node.color = Color.RED;
+        return rightChild;
+    }
+
+    private void colorSwap(Node node) {
+        node.rightChild.color = Color.BLACK;
+        node.leftChild.color = Color.BLACK;
+        node.color = Color.RED;
+    }
+
+    private enum Color {
+        RED, BLACK
+    }
+}
